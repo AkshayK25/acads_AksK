7c258767446ab46cdcd97aa4a3e6c08ce8598df1
AkshayK25 <39946049+AkshayK25@users.noreply.github.com>
2018-07-10 21:22:14 +0530

    Add files via upload

---
+++ Numpie_python.py
@@ -0,0 +1,43 @@
+import numpy as np
+import math
+
+# Question 1
+
+print("--------------------------------------Playing with Numpy------------------------------------------------------")
+x = np.random.rand(10, 1)
+print(x)
+print("Mean of x ->", x.mean())
+print("-----------------------------------------------------------------------------------------------")
+
+# Question 2
+print("-----------------------------------------------------------------------------------------------")
+y = np.random.rand(20, 1)
+print(y)
+print("Standard_deviation of y ->", y.std())
+print("Variance of y is ->", np.var(y, ddof=1))
+print("-----------------------------------------------------------------------------------------------")
+
+# Question 4
+
+
+def funk(k):
+    f = 1 / (1 + math.exp(-k))
+    r = np.full((10, 1), f)
+    print(r)
+    print("Shape of Matrix_r ->", np.shape(r))
+    print("--------------------------------------------------------------------------------------------")
+
+
+funk(2)
+
+# Question 3
+print("------------------------------------------------------------------------------------------------")
+matrix1 = np.random.random((10, 20))
+print(matrix1)
+matrix2 = np.random.random((20, 25))
+print(matrix2)
+matrix3 = np.dot(matrix1, matrix2)
+print("Dot_product of 2 matrices ->", matrix3)
+print("Shape of new Matrix:", np.shape(matrix3))
+print("Sum of all the elements of new matrix:", np.sum(matrix3))
+print("----------------------End of the program--------------------------------------------------------")
